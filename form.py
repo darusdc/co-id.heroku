765ee6592ede965ecd13dad6f61915b35c03dd43
Muhammad Darussalam <68312491+darusdc@users.noreply.github.com>
2021-07-02 18:41:41 +0700

    Delete form.py

--- form.py
+++
@@ -1,9 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired, Length,Email,EqualTo
-
-class RegistrationForm(FlaskForm):
-    napang=StringField('Nama Pangkalan',
-    validators=[DataRequired(), Length(min=3, max=25)])
-    email=StringField('Email', validators=[DataRequired()])
-    submit=SubmitField('sign up')
